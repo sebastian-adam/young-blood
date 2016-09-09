1f8af9224cf33bfca1ddf43e4a31c34bf63ae191
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-09-08 22:12:21 -0700

    render music videos on index in order of location

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass'
 gem 'haml'
+gem 'simple_form'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- app/controllers/music_videos_controller.rb
+++ app/controllers/music_videos_controller.rb
@@ -1,4 +1,18 @@
 class MusicVideosController < ApplicationController
   def index
+    @music_videos = MusicVideo.order(:location)
+  end
+  def new
+    @music_video = MusicVideo.new
+  end
+  def create
+    @music_video = MusicVideo.create(music_video_params)
+    redirect_to music_videos_path
+  end
+
+  private
+
+  def music_video_params
+    params.require(:music_video).permit(:title, :artist, :location, :year, :link)
   end
 end
