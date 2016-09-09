1399ff2631fe9a28ca378b876f91e1f50fc73450
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-09-09 00:46:28 -0700

    add destroy action to music video controller

--- app/controllers/music_videos_controller.rb
+++ app/controllers/music_videos_controller.rb
@@ -2,23 +2,34 @@ class MusicVideosController < ApplicationController
   def index
     @music_videos = MusicVideo.order(:location)
   end
+
   def new
     @music_video = MusicVideo.new
   end
+
   def create
     @music_video = MusicVideo.create(music_video_params)
     redirect_to music_videos_path
   end
+
   def show
     @music_video = MusicVideo.find(params[:id])
   end
+
   def edit
     @music_video = MusicVideo.find(params[:id])
   end
+
   def update
     @music_video = MusicVideo.find(params[:id])
-    @music_video.update(music_videos_params)
+    @music_video.update(music_video_params)
     redirect_to music_videos_path(@music_video)
+  end
+
+  def destroy
+    @music_video = MusicVideo.find(params[:id])
+    @music_video.destroy
+    redirect_to music_videos_path
   end
 
   private
