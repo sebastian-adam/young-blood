14387000b77fc2f1d2a3d10b7e5bdb810756a1f3
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-09-11 01:41:17 -0700

    introduce sticky city title

--- app/assets/javascripts/sticky_nav.js
+++ app/assets/javascripts/sticky_nav.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
   $("#young_blood_header").sticky({topSpacing:0});
-  $(".city_title").sticky({topSpacing:200});
+  for (i = 10; i < 50; i++) {
+    $("#" + i + "_city").sticky({topSpacing:180});
+  }
 });
--- app/assets/javascripts/video_carousel.js
+++ app/assets/javascripts/video_carousel.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   for (i = 10; i < 50; i++) {
-    $("#" + i).owlCarousel({
+    $("#" + i + "_carousel").owlCarousel({
       navigation : false, // Show next and prev buttons
       slideSpeed : 400,
       paginationSpeed : 400,
