ae3fde3c513a4b4735ce2c87cd4d08aba98e4c90
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-09-25 18:32:06 -0700

    update alphabet markers to change css on scroll

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
     }
 
     for (i = 10; i < 150; i++) {
-      $("#" + i + "_city").sticky({topSpacing:200});
+      $("#" + i + "_city").sticky({topSpacing:192});
     }
 
     $('#advanced-toggle').on("click", function() {
@@ -36,8 +36,33 @@ $(document).ready(function() {
       window.scrollTo( 0, position.top - 200)
     });
 
+    var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    var alphabet_positions = [];
+    alphabet.forEach(function(i) {
+      position = $('.' + i + '-char').first().offset();
+      if(position) {
+        alphabet_positions.push(position.top - 201);
+      } else {
+        alphabet_positions.push(99999);
+      }
+    });
+    $(window).scroll(function() {
+      var scroll_position = $(window).scrollTop();
+      for(i = 0; i < 25; i++) {
+        if(scroll_position > alphabet_positions[i]) {
+          $(".alphabet-marker").css({
+            'color': '#1edbcc', 'opacity': '0.3'
+          });
+          $("#" + alphabet[i] + "-char").css({
+            'color': 'gold', 'opacity': '1'
+          });
+        }
+      }
+    });
+
   } else {
     $('.tags').hide();
+    $('#alphabet-container').hide();
     $('#my-nav').removeClass('navbar-fixed-top');
     $('#content_container').css({'margin-top' : '0'});
   };
