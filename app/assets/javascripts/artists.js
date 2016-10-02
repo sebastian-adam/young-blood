e9e6d441e73cdcea6de67ab885589b1b01cbbb5b
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-10-02 00:19:10 -0700

    fix city titles to update between filter transitions

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
     $(".pagination-marker").on("click", function(){
         var carousel_id = $(this).siblings('.owl-carousel').attr('id');
         var carousel_number = carousel_id.split('-')[0];
-        var pagination_id = $(this).attr('id');
+        var pagination_id = $(this).attr('pagination');
         var pagination_position = pagination_id.split('-')[0];
 
         var adjust_for_hidden = $(this).prevAll('.pagination-marker:hidden').length;
@@ -34,7 +34,7 @@ $(document).ready(function() {
     });
 
     for (i = 0; i < number_of_artists; i++) {
-      $("#" + i + "-city-title").sticky({topSpacing:192});
+      $("#" + i + "-city-title").sticky({topSpacing:180});
     }
 
     $('#advanced-toggle').on("click", function() {
@@ -53,65 +53,122 @@ $(document).ready(function() {
 
     $('#remove-filter').on("click", function() {
       year = false;
-      $('.carousel-tile').show();
-      $('.artist').show();
-      $('.pagination-marker').show()
-      $('#filter-header').hide()
-      $('#alphabet-container').show()
+      $('.carousel-tile').removeClass('hidden');
+      $('.artist').removeClass('hidden');
+      $('.pagination-marker').removeClass('hidden')
+      $('#filter-header').addClass('hidden')
+      $('#alphabet-container').removeClass('hidden')
       window.scrollTo( 0, 0)
+      $('.city-title').removeClass('hidden');
+      var previousCity = 'nil';
+      var currentcity = 'nil';
+      $.each($('.city-title:visible'), function() {
+        var currentCity = $(this).attr('city');
+        if (currentCity == previousCity) {
+          $(this).addClass('hidden');
+          $(this).parents('.music-video-tile').siblings('.city-divider').addClass('hidden');
+        } else {
+          $(this).removeClass('hidden');
+          $(this).parents('.music-video-tile').siblings('.city-divider').removeClass('hidden');
+        }
+        previousCity = currentCity;
+      });
     });
 
     $('.vibe').on("click", function(e) {
       year = false;
-      $('.carousel-tile').show();
-      $('.pagination-marker').show()
+      $('.carousel-tile').removeClass('hidden');
+      $('.pagination-marker').removeClass('hidden')
       var vibe = $(this).attr('vibe');
-      $('.artist').not('.' + vibe).hide();
-      $('.' + vibe).show();
+      $('.artist').not('.' + vibe).addClass('hidden');
+      $('.' + vibe).removeClass('hidden');
       $('#filter-title').html(vibe);
+      $('.city-title').removeClass('hidden');
+      var previousCity = 'nil';
+      var currentcity = 'nil';
+      $.each($('.city-title:visible'), function() {
+        var currentCity = $(this).attr('city');
+        if (currentCity == previousCity) {
+          $(this).addClass('hidden');
+          $(this).parents('.music-video-tile').siblings('.city-divider').addClass('hidden');
+        } else {
+          $(this).removeClass('hidden');
+          $(this).parents('.music-video-tile').siblings('.city-divider').removeClass('hidden');
+        }
+        previousCity = currentCity;
+      });
     });
 
     $('.region').on("click", function() {
       year = false;
-      $('.carousel-tile').show();
-      $('.pagination-marker').show()
+      $('.carousel-tile').removeClass('hidden');
+      $('.pagination-marker').removeClass('hidden')
       var region = $(this).attr('region');
-      $('.artist').not('.' + region).hide();
-      $('.' + region).show();
+      $('.artist').not('.' + region).addClass('hidden');
+      $('.' + region).removeClass('hidden');
       $('#filter-title').html(region);
+      $('.city-title').removeClass('hidden');
+      var previousCity = 'nil';
+      var currentcity = 'nil';
+      $.each($('.city-title:visible'), function() {
+        var currentCity = $(this).attr('city');
+        if (currentCity == previousCity) {
+          $(this).addClass('hidden');
+          $(this).parents('.music-video-tile').siblings('.city-divider').addClass('hidden');
+        } else {
+          $(this).removeClass('hidden');
+          $(this).parents('.music-video-tile').siblings('.city-divider').removeClass('hidden');
+        }
+        previousCity = currentCity;
+      });
     });
 
-    $('.city').on("click", function() {
-      year = false;
-      $('.carousel-tile').show();
-      $('.pagination-marker').show()
-      var city = $(this).attr('city');
-      $('.artist').not('.' + city).hide();
-      $('.' + city).show();
-      $('#filter-title').html(city);
-    });
+    // $('.city').on("click", function() {
+    //   year = false;
+    //   $('.carousel-tile').removeClass('hidden');
+    //   $('.pagination-marker').removeClass('hidden')
+    //   var city = $(this).attr('city');
+    //   $('.artist').not('.' + city).addClass('hidden');
+    //   $('.' + city).removeClass('hidden');
+    //   $('#filter-title').html(city);
+    // });
 
     $('.year').on("click", function() {
-      $('.carousel-tile').show();
-      $('.pagination-marker').show()
+      $('.carousel-tile').removeClass('hidden');
+      $('.pagination-marker').removeClass('hidden')
       var year = $(this).attr('year');
-      $('.artist').not('.' + year).hide();
-      $('.' + year).show();
+      $('.artist').not('.' + year).addClass('hidden');
+      $('.' + year).removeClass('hidden');
       $('#filter-title').html(year);
 
-      $('.carousel-tile').not('.' + year).hide();
+      $('.carousel-tile').not('.' + year).addClass('hidden');
       $('.owl-carousel').trigger('owl.jumpTo', 0);
 
       $.each($('.artist:visible .carousel-tile:hidden'), function() {
         var carousel_tile_number = $(this).attr('id').split('-')[0];
         var carousel_artist = $(this).attr('artist');
-        $('.pagination-marker[pagination="' + carousel_tile_number + '"][artist="' + carousel_artist + '"]').hide();
+        $('.pagination-marker[pagination="' + carousel_tile_number + '"][artist="' + carousel_artist + '"]').addClass('hidden');
       });
 
       $.each($('.center-column-body'), function() {
         if($(this).children('button:visible').length == 1) {
-          $(this).children('button:visible').hide();
+          $(this).children('button:visible').addClass('hidden');
         }
+      });
+
+      $('.city-title').removeClass('hidden');
+      var previousCity = 'nil';
+      var currentcity = 'nil';
+      $.each($('.city-title:visible'), function() {
+        var currentCity = $(this).attr('city');
+        if (currentCity == previousCity) {
+          $(this).addClass('hidden');
+          $(this).parents('.music-video-tile').siblings('.city-divider').addClass('hidden');
+        } else {
+          $(this).removeClass('hidden');
+          $(this).parents('.music-video-tile').siblings('.city-divider').removeClass('hidden');
+        }
+        previousCity = currentCity;
       });
     });
 
@@ -121,25 +178,25 @@ $(document).ready(function() {
       var year = $("input[name=year]:checked").val() ?
       $("input[name=year]:checked").val() : 'artist';
 
-      $('.carousel-tile').show();
-      $('.owl-page').show();
-      $('.artist').show();
-      $('.artist').not('.' + vibe + '.' + region + '.' + year).hide();
+      $('.carousel-tile').removeClass('hidden');
+      $('.owl-page').removeClass('hidden');
+      $('.artist').removeClass('hidden');
+      $('.artist').not('.' + vibe + '.' + region + '.' + year).addClass('hidden');
 
       if (year && year != 'artist') {
-        $('.carousel-tile').not('.' + year).hide();
+        $('.carousel-tile').not('.' + year).addClass('hidden');
         $('.owl-carousel').trigger('owl.jumpTo', 0);
 
         $.each($('.artist:visible .carousel-tile:hidden'), function() {
           var carousel_tile_number = $(this).attr('id').split('-')[0];
           var carousel_artist = $(this).attr('artist');
 
-          $('.pagination-marker[pagination="' + carousel_tile_number + '"][artist="' + carousel_artist + '"]').hide();
+          $('.pagination-marker[pagination="' + carousel_tile_number + '"][artist="' + carousel_artist + '"]').addClass('hidden');
         });
 
         $.each($('.center-column-body'), function() {
           if($(this).children('button:visible').length == 1) {
-            $(this).children('button:visible').hide();
+            $(this).children('button:visible').addClass('hidden');
           }
         });
       }
@@ -160,14 +217,14 @@ $(document).ready(function() {
 
     $('.alphabet-marker').on('click', function() {
       var alphabet_character = $(this).attr('id').split('-')[0]
-      position = $('#' + alphabet_character + '-position').first().offset();
-      window.scrollTo( 0, position.top - 200)
+      position = $('.' + alphabet_character + '-position:first').offset();
+      $('html, body').animate({scrollTop: position.top - 200}, 1000);
     });
 
     var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
     var alphabet_positions = [];
     alphabet.forEach(function(i) {
-      position = $('#' + i + '-position').first().offset();
+      position = $('.' + i + '-position:first').offset();
       if(position) {
         alphabet_positions.push(position.top - 201);
       } else {
@@ -191,8 +248,8 @@ $(document).ready(function() {
 
 
   } else {
-    $('.tags').hide();
-    $('#alphabet-container').hide();
+    $('.tags').addClass('hidden');
+    $('#alphabet-container').addClass('hidden');
     $('#my-nav').removeClass('navbar-fixed-top');
     $('#content_container').css({'margin-top' : '0'});
   };
