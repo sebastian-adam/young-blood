76754ccd583385ddfe9da0083647a3fa44d6d5f7
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-10-05 14:54:20 -0700

    adapt subplaylists into singular Playlist object to prevent track repeat and associated issues

--- app/assets/javascripts/music_video.js
+++ app/assets/javascripts/music_video.js
@@ -1,5 +1,4 @@
-/* iFrame Lazy Loader inspired by @labnol */
-/* Web: http://labnol.org/?p=27941 */
+/* iFrame Lazy Loader inspired by @labnol :Web: http://labnol.org/?p=27941 */
 
 
 // Build Lazy Loader assets
@@ -30,6 +29,7 @@ function buildIframe() {
   initializedVids.push(player);
 }
 
+// Shuffle playlist method
 function shuffle(array) {
   var m = array.length, t, i;
   // While there remain elements to shuffle…
@@ -44,33 +44,202 @@ function shuffle(array) {
   return array;
 }
 
-// Playlist object methods
+// Playlist constructor
 function Playlist(filter) {
-  this.playlist = [];
-  this.playCount = 0;
+  this.main = [];
   this.filter = filter;
+
+  this.thug = [];
+  this.soul = [];
+  this.wavy = [];
+  this.chill = [];
+  this.pop = [];
+  this.punk = [];
+  this.indie = [];
+  this.thump = [];
+
+  this.east = [];
+  this.west = [];
+  this.south = [];
+  this.midwest = [];
+  this.other = [];
+
+  this.thirteen = [];
+  this.fourteen = [];
+  this.fifteen = [];
+  this.sixteen = [];
 }
 
+// Populate playlist arrays
 Playlist.prototype.collectVideos = function() {
-  var videos = [];
-  var videoCount = 0;
-  if (this.filter == "2010" || this.filter == "2011" || this.filter == "2012" || this.filter == "2013" || this.filter == "2014" || this.filter == "2015" || this.filter == "2016") {
-    $('.' + this.filter).find('.carousel-tile.' + this.filter).find('.youtube-player div[data-id]').each(function() {
-      videos.push($(this).attr('id'));
-      videoCount += 1;
-    });
-  } else {
-    $('.' + this.filter).find('.youtube-player div[data-id]').each(function() {
-      videos.push($(this).attr('id'));
-      videoCount += 1;
-    });
-  }
-  return videos;
+  var main = this.main
+  var thug = this.thug;
+  var soul = this.soul;
+  var wavy = this.wavy;
+  var chill = this.chill;
+  var pop = this.pop;
+  var punk = this.punk;
+  var indie = this.indie;
+  var thump = this.thump;
+  var east = this.east;
+  var west = this.west;
+  var south = this.south;
+  var midwest = this.midwest;
+  var other = this.other;
+  var thirteen = this.thirteen;
+  var fourteen = this.fourteen;
+  var fifteen = this.fifteen;
+  var sixteen = this.sixteen;
+
+  $.each($('div[artist]'), function() {
+    var vibe = $(this).attr('artist_vibe');
+    var region = $(this).attr('artist_region');
+    var year = $(this).attr('year');
+    var play_btn_id = $(this).find('.youtube-player div[data-id]').attr('id');
+
+    var allVideo = play_btn_id;
+    main.push(allVideo)
+
+    if (vibe == 'thug') {
+      var thugVideo = play_btn_id;
+      thug.push(thugVideo)
+    } else if (vibe == 'soul') {
+      var soulVideo = play_btn_id;
+      soul.push(soulVideo)
+    } else if (vibe == 'wavy') {
+      var wavyVideo = play_btn_id;
+      wavy.push(wavyVideo)
+    } else if (vibe == 'chill') {
+      var chillVideo = play_btn_id;
+      chill.push(chillVideo)
+    } else if (vibe == 'pop') {
+      var popVideo = play_btn_id;
+      pop.push(popVideo)
+    } else if (vibe == 'punk') {
+      var punkVideo = play_btn_id;
+      punk.push(punkVideo)
+    } else if (vibe == 'indie') {
+      var indieVideo = play_btn_id;
+      indie.push(indieVideo)
+    } else if (vibe == 'thump') {
+      var thumpVideo = play_btn_id;
+      thump.push(thumpVideo)
+    }
+
+    if (region == 'east') {
+      var eastVideo = play_btn_id;
+      east.push(eastVideo)
+    } else if (region == 'west') {
+      var westVideo = play_btn_id;
+      west.push(westVideo)
+    } else if (region == 'south') {
+      var southVideo = play_btn_id;
+      south.push(southVideo)
+    } else if (region == 'midwest') {
+      var midwestVideo = play_btn_id;
+      midwest.push(midwestVideo)
+    } else if (region == 'other') {
+      var otherVideo = play_btn_id;
+      other.push(otherVideo)
+    }
+
+    if (year == '2013') {
+      var thirteenVideo = play_btn_id;
+      thirteen.push(thirteenVideo)
+    } else if (year == '2014') {
+      var fourteenVideo = play_btn_id;
+      fourteen.push(fourteenVideo)
+    } else if (year == '2015') {
+      var fifteenVideo = play_btn_id;
+      fifteen.push(fifteenVideo)
+    } else if (year == '2016') {
+      var sixteenVideo = play_btn_id;
+      sixteen.push(sixteenVideo)
+    }
+  });
+  // Shuffle playlists
+  this.main = shuffle(this.main);
+  this.thug = shuffle(this.thug);
+  this.soul = shuffle(this.soul);
+  this.wavy = shuffle(this.wavy);
+  this.chill = shuffle(this.chill);
+  this.pop = shuffle(this.pop);
+  this.punk = shuffle(this.punk);
+  this.indie = shuffle(this.indie);
+  this.thump = shuffle(this.thump);
+  this.east = shuffle(this.east);
+  this.west = shuffle(this.west);
+  this.south = shuffle(this.south);
+  this.midwest = shuffle(this.midwest);
+  this.other = shuffle(this.other);
+  this.thirteen = shuffle(this.thirteen);
+  this.fourteen = shuffle(this.fourteen);
+  this.fifteen = shuffle(this.fifteen);
+  this.sixteen = shuffle(this.sixteen);
 }
 
+// Advance playlist
 Playlist.prototype.nextVideo = function() {
-  var nextVideoId = this.playlist[this.playCount];
-  this.playCount += 1;
+  if (this.filter == 'artist') {
+    var nextVideoId = this.main[0];
+  } else if (this.filter == 'thug') {
+    var nextVideoId = this.thug[0];
+    this.thug.shift();
+  } else if (this.filter == 'soul') {
+    var nextVideoId = this.soul[0];
+    this.soul.shift();
+  } else if (this.filter == 'wavy') {
+    var nextVideoId = this.wavy[0];
+    this.wavy.shift();
+  } else if (this.filter == 'chill') {
+    var nextVideoId = this.chill[0];
+    this.chill.shift();
+  } else if (this.filter == 'pop') {
+    var nextVideoId = this.pop[0];
+    this.pop.shift();
+  } else if (this.filter == 'punk') {
+    var nextVideoId = this.punk[0];
+    this.punk.shift();
+  } else if (this.filter == 'indie') {
+    var nextVideoId = this.indie[0];
+    this.indie.shift();
+  } else if (this.filter == 'thump') {
+    var nextVideoId = this.thump[0];
+    this.thump.shift();
+  } else if (this.filter == 'soul') {
+    var nextVideoId = this.soul[0];
+    this.soul.shift();
+  } else if (this.filter == 'east') {
+    var nextVideoId = this.east[0];
+    this.east.shift();
+  } else if (this.filter == 'west') {
+    var nextVideoId = this.west[0];
+    this.west.shift();
+  } else if (this.filter == 'south') {
+    var nextVideoId = this.south[0];
+    this.south.shift();
+  } else if (this.filter == 'midwest') {
+    var nextVideoId = this.midwest[0];
+    this.midwest.shift();
+  } else if (this.filter == 'other') {
+    var nextVideoId = this.other[0];
+    this.other.shift();
+  } else if (this.filter == 'thirteen') {
+    var nextVideoId = this.thirteen[0];
+    this.thirteen.shift();
+  } else if (this.filter == 'fourteen') {
+    var nextVideoId = this.fourteen[0];
+    this.fourteen.shift();
+  } else if (this.filter == 'fifteen') {
+    var nextVideoId = this.fifteen[0];
+    this.fifteen.shift();
+  } else if (this.filter == 'sixteen') {
+    var nextVideoId = this.sixteen[0];
+    this.sixteen.shift();
+  }
+
+  index = this.main.indexOf(nextVideoId);
+  this.main.splice(index, 1);
 
   $('html, body').animate({scrollTop: $('#' + nextVideoId).parents('.center-column-body').siblings('.side-column-body').find('.alphabet-marker').offset().top - 120}, 2000);
 
@@ -96,32 +265,11 @@ Playlist.prototype.nextVideo = function() {
     $('#' + nextVideoId).parents('.owl-carousel').siblings('.artist-title').children('a').click();
 
   }, 2000);
+
 }
 
-// Create Playlists and ready environment
-var playlistType = 'artist';
-
-var artistPlaylist = new Playlist('artist');
-var thugPlaylist = new Playlist('thug');
-var soulPlaylist = new Playlist('soul');
-var wavyPlaylist = new Playlist('wavy');
-var chillPlaylist = new Playlist('chill');
-var popPlaylist = new Playlist('pop');
-var punkPlaylist = new Playlist('punk');
-var indiePlaylist = new Playlist('indie');
-var thumpPlaylist = new Playlist('thump');
-var eastPlaylist = new Playlist('east');
-var westPlaylist = new Playlist('west');
-var southPlaylist = new Playlist('south');
-var midwestPlaylist = new Playlist('midwest');
-var globalPlaylist = new Playlist('global');
-var olderPlaylist = new Playlist('2012');
-var thirteenPlaylist = new Playlist('2013');
-var fourteenPlaylist = new Playlist('2014');
-var fifteenPlaylist = new Playlist('2015');
-var sixteenPlaylist = new Playlist('2016');
-
-
+// Create playlist with subplaylists and set filter type
+var youngBloodPlaylist = new Playlist('artist');
 
 // Youtube iframe API methods
 function onPlayerReady(event) {
@@ -132,43 +280,7 @@ function onPlayerStateChange(event) {
   if (event.target.getPlayerState() == YT.PlayerState.PLAYING) {
     currentVid = event.target.getVideoData()['video_id'];
   } else if (event.target.getPlayerState() == YT.PlayerState.ENDED) {
-    if (playlistType == 'artist') {
-      artistPlaylist.nextVideo();
-    } else if (playlistType == 'thug') {
-      thugPlaylist.nextVideo();
-    } else if (playlistType == 'soul') {
-      soulPlaylist.nextVideo();
-    } else if (playlistType == 'wavy') {
-      wavyPlaylist.nextVideo();
-    } else if (playlistType == 'chill') {
-      chillPlaylist.nextVideo();
-    } else if (playlistType == 'pop') {
-      popPlaylist.nextVideo();
-    } else if (playlistType == 'punk') {
-      punkPlaylist.nextVideo();
-    } else if (playlistType == 'indie') {
-      indiePlaylist.nextVideo();
-    } else if (playlistType == 'thump') {
-      thumpPlaylist.nextVideo();
-    } else if (playlistType == 'east') {
-      eastPlaylist.nextVideo();
-    } else if (playlistType == 'west') {
-      westPlaylist.nextVideo();
-    } else if (playlistType == 'south') {
-      southPlaylist.nextVideo();
-    } else if (playlistType == 'midwest') {
-      midwestPlaylist.nextVideo();
-    } else if (playlistType == 'older') {
-      olderPlaylist.nextVideo();
-    } else if (playlistType == 'thirteen') {
-      thirteenPlaylist.nextVideo();
-    } else if (playlistType == 'fourteen') {
-      fourteenPlaylist.nextVideo();
-    } else if (playlistType == 'fifteen') {
-      fifteenPlaylist.nextVideo();
-    } else if (playlistType == 'sixteen') {
-      sixteenPlaylist.nextVideo();
-    }
+    youngBloodPlaylist.nextVideo(currentPlaylist);
   }
 
   initializedVids.forEach(function(vid) {
@@ -199,148 +311,86 @@ $(function() {
     allCounter += 1;
   });
 
-  // Build main playlist
-  var artistVideos = artistPlaylist.collectVideos();
-  artistPlaylist.playlist = shuffle(artistVideos);
+  // Build and shuffle playlists
+  var youngBloodVideos = youngBloodPlaylist.collectVideos();
 
   // Main playlist controls
   $('#playlist-start').on('click', function() {
-    artistPlaylist.nextVideo();
-    playlistType = 'artist';
+    youngBloodPlaylist.filter = 'artist';
+    youngBloodPlaylist.nextVideo();
   });
-
-  // Build playlist for each vibe
-  var thugVideos = thugPlaylist.collectVideos();
-  thugPlaylist.playlist = shuffle(thugVideos);
-
-  var soulVideos = soulPlaylist.collectVideos();
-  soulPlaylist.playlist = shuffle(soulVideos);
-
-  var wavyVideos = wavyPlaylist.collectVideos();
-  wavyPlaylist.playlist = shuffle(wavyVideos);
-
-  var chillVideos = chillPlaylist.collectVideos();
-  chillPlaylist.playlist = shuffle(chillVideos);
-
-  var popVideos = popPlaylist.collectVideos();
-  popPlaylist.playlist = shuffle(popVideos);
-
-  var punkVideos = punkPlaylist.collectVideos();
-  punkPlaylist.playlist = shuffle(punkVideos);
-
-  var indieVideos = indiePlaylist.collectVideos();
-  indiePlaylist.playlist = shuffle(indieVideos);
-
-  var thumpVideos = thumpPlaylist.collectVideos();
-  thumpPlaylist.playlist = shuffle(thumpVideos);
-
-  // Build playlist for each region
-  var eastVideos = eastPlaylist.collectVideos();
-  eastPlaylist.playlist = shuffle(eastVideos);
-
-  var westVideos = westPlaylist.collectVideos();
-  westPlaylist.playlist = shuffle(westVideos);
-
-  var southVideos = southPlaylist.collectVideos();
-  southPlaylist.playlist = shuffle(southVideos);
-
-  var midwestVideos = midwestPlaylist.collectVideos();
-  midwestPlaylist.playlist = shuffle(midwestVideos);
-
-  var globalVideos = globalPlaylist.collectVideos();
-  globalPlaylist.playlist = shuffle(globalVideos);
-
-  // Build playlist for each year
-  var olderVideos = olderPlaylist.collectVideos();
-  olderPlaylist.playlist = shuffle(olderVideos);
-
-  var thirteenVideos = thirteenPlaylist.collectVideos();
-  thirteenPlaylist.playlist = shuffle(thirteenVideos);
-
-  var fourteenVideos = fourteenPlaylist.collectVideos();
-  fourteenPlaylist.playlist = shuffle(fourteenVideos);
-
-  var fifteenVideos = fifteenPlaylist.collectVideos();
-  fifteenPlaylist.playlist = shuffle(fifteenVideos);
-
-  var sixteenVideos = sixteenPlaylist.collectVideos();
-  sixteenPlaylist.playlist = shuffle(sixteenVideos);
 
   // Vibe playlist controls
   $('#thug-playlist-start').on('click', function() {
-    thugPlaylist.nextVideo();
-    playlistType = 'thug';
+    youngBloodPlaylist.filter = 'thug';
+    youngBloodPlaylist.nextVideo();
   });
   $('#soul-playlist-start').on('click', function() {
-    soulPlaylist.nextVideo();
-    playlistType = 'soul';
-  });
+    youngBloodPlaylist.filter = 'soul';
+    youngBloodPlaylist.nextVideo();
+    });
   $('#wavy-playlist-start').on('click', function() {
-    wavyPlaylist.nextVideo();
-    playlistType = 'wavy';
-  });
+    youngBloodPlaylist.filter = 'wavy';
+    youngBloodPlaylist.nextVideo();
+    });
   $('#chill-playlist-start').on('click', function() {
-    chillPlaylist.nextVideo();
-    playlistType = 'chill';
-  });
+    youngBloodPlaylist.filter = 'chill';
+    youngBloodPlaylist.nextVideo();
+    });
   $('#pop-playlist-start').on('click', function() {
-    popPlaylist.nextVideo();
-    playlistType = 'pop';
-  });
+    youngBloodPlaylist.filter = 'pop';
+    youngBloodPlaylist.nextVideo();
+    });
   $('#punk-playlist-start').on('click', function() {
-    punkPlaylist.nextVideo();
-    playlistType = 'punk';
-  });
+    youngBloodPlaylist.filter = 'punk';
+    youngBloodPlaylist.nextVideo();
+    });
   $('#indie-playlist-start').on('click', function() {
-    indiePlaylist.nextVideo();
-    playlistType = 'indie';
-  });
+    youngBloodPlaylist.filter = 'indie';
+    youngBloodPlaylist.nextVideo();
+    });
   $('#thump-playlist-start').on('click', function() {
-    thumpPlaylist.nextVideo();
-    playlistType = 'thump';
-  });
+    youngBloodPlaylist.filter = 'thump';
+    youngBloodPlaylist.nextVideo();
+    });
 
   // Region playlist controls
   $('#east-playlist-start').on('click', function() {
-    eastPlaylist.nextVideo();
-    playlistType = 'east';
-  });
+    youngBloodPlaylist.filter = 'east';
+    youngBloodPlaylist.nextVideo();
+    });
   $('#west-playlist-start').on('click', function() {
-    westPlaylist.nextVideo();
-    playlistType = 'west';
-  });
+    youngBloodPlaylist.filter = 'west';
+    youngBloodPlaylist.nextVideo();
+    });
   $('#south-playlist-start').on('click', function() {
-    southPlaylist.nextVideo();
-    playlistType = 'south';
-  });
+    youngBloodPlaylist.filter = 'south';
+    youngBloodPlaylist.nextVideo();
+    });
   $('#midwest-playlist-start').on('click', function() {
-    midwestPlaylist.nextVideo();
-    playlistType = 'midwest';
-  });
+    youngBloodPlaylist.filter = 'midwest';
+    youngBloodPlaylist.nextVideo();
+    });
   $('#global-playlist-start').on('click', function() {
-    globalPlaylist.nextVideo();
-    playlistType = 'global';
-  });
+    youngBloodPlaylist.filter = 'other';
+    youngBloodPlaylist.nextVideo();
+    });
 
   // Year playlist controls
-  $('#older-playlist-start').on('click', function() {
-    olderPlaylist.nextVideo();
-    playlistType = 'older';
-  });
   $('#thirteen-playlist-start').on('click', function() {
-    thirteenPlaylist.nextVideo();
-    playlistType = 'thirteen';
-  });
+    youngBloodPlaylist.filter = 'thirteen';
+    youngBloodPlaylist.nextVideo();
+    });
   $('#fourteen-playlist-start').on('click', function() {
-    fourteenPlaylist.nextVideo();
-    playlistType = 'fourteen';
-  });
+    youngBloodPlaylist.filter = 'fourteen';
+    youngBloodPlaylist.nextVideo();
+    });
   $('#fifteen-playlist-start').on('click', function() {
-    fifteenPlaylist.nextVideo();
-    playlistType = 'fifteen';
-  });
+    youngBloodPlaylist.filter = 'fifteen';
+    youngBloodPlaylist.nextVideo();
+    });
   $('#sixteen-playlist-start').on('click', function() {
-    sixteenPlaylist.nextVideo();
-    playlistType = 'sixteen';
-  });
+    youngBloodPlaylist.filter = 'sixteen';
+    youngBloodPlaylist.nextVideo();
+    });
 });
